e26b9b982c853b84a3fce98ab10ee88683f58023
moshiur9185 <moshiur9185@gmail.com>
2021-05-10 17:38:46 +0600

    work update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fileUpload = require("express-fileupload");
 const MongoClient = require('mongodb').MongoClient;
 require("dotenv").config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vakyo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r03xf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const app = express();
 
 app.use(express.json());
